bf4f970ebda937595e0a05d67d11cd627f235ca1
petejh <petejh@truly.fake>
2014-03-02 19:58:58 +0000

    Create redux project
    This is intended to be a very spare project to re-create a pale emulation
    of the Wikiful and Twetter apps using a BDD methodology. Specifically, per
    the curriculum, we'll use an inside-out approach.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Redux::Application.config.secret_key_base = 'a995ff5c65794906450c3f379a44b2cf24097d083b00a86f30640bc044aa640a561990fa47de28e599e8318ec62723b62bc4f0628d63a40757260315f661e990'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Redux::Application.config.session_store :cookie_store, key: '_redux_session'
