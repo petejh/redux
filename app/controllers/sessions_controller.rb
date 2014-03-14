c77e586af6275b640706b3ecb08879da9c675914
petejh <petejh@truly.fake>
2014-03-14 21:20:25 +0000

    Drive out SessionsController
    These specs still need some serious refactoring to DRY them out.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
   def create
-    @user = User.find_by login: params[:login]
-    session[:user_id] = @user.id
+    user = User.where(login: params[:login]).first
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
     redirect_to root_url
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -14,23 +14,61 @@ describe SessionsController do
       let!(:valid_user) { FactoryGirl.create(:user, credentials) }
       
       it "finds the user" do
-        # make this go away and switch @user back to a local variable once the "stores the session ID" test passes
+        pending("figure out how to write this spec without requiring the application code to use an instance variable")
+#        User.should_receive(:where).with({:login=>"#{credentials[:login]}"})
         post :create, credentials
-        expect(assigns(:user)).to_not eq(nil)
+        expect(assigns(:user)).to eq(valid_user)
       end
 
       it "stores the session ID" do
-        pending
         post :create, credentials
-        expect(assigns(session[:user_id])).to eq(valid_user.id)
+        # is this expectation doing what I think it's doing?
+        # why does this work with the request object, but not the response object?
+        expect(request.session[:user_id]).to eq(valid_user.id)
       end
       
       it "redirects to the application root" do
         expect(post :create, credentials).to redirect_to root_url
       end
     end
+    
+    context "when credentails are invalid" do
+      context "when the user login can't be found" do
+        let(:credentials) { FactoryGirl.attributes_for(:user, login: 'schnecky') }
+        let(:wrong_login) { FactoryGirl.attributes_for(:user, login: 'strudemeier') }
+        let!(:valid_user) { FactoryGirl.create(:user, credentials) }
+        
+        it "renders the user signin form" do
+          expect(post :create, wrong_login).to render_template :new
+        end
+      end
+
+      context "when the user's password can't be authenticated" do
+        let(:credentials) { FactoryGirl.attributes_for(:user, login: 'schnecky', password: 'abcd1234', password_confirmation: 'abcd1234') }
+        let(:wrong_password) { FactoryGirl.attributes_for(:user, login: 'schnecky', password: 'wxyz5678', password_confirmation: 'wxyz5678') }
+        let!(:valid_user) { FactoryGirl.create(:user, credentials) }
+        
+        it "renders the user signin form" do
+          expect(post :create, wrong_password).to render_template :new
+        end
+      end
+    end
   end
 
   describe "DELETE destroy" do
+    let(:credentials) { FactoryGirl.attributes_for(:user) }
+    let!(:valid_user) { FactoryGirl.create(:user, credentials) }
+    
+    it "clears the session ID" do
+      post :create, credentials
+      delete :destroy, id: valid_user.id
+      # is this expectation doing what I think it's doing?
+      # why does this work with the request object, but not the response object?
+      expect(request.session[:user_id]).to eq(nil)
+    end
+    
+    it "redirects to the application root" do
+      expect(delete :destroy, id: valid_user.id).to redirect_to root_url
+    end
   end
 end
